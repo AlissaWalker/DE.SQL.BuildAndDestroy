3a34c622c92d4b7478735c60b2c90161112b2d50
Alissa Walker <99627604+AlissaWalker@users.noreply.github.com>
2022-03-23 14:13:58 -0400

    Update exercise10.sql

---
+++ answers/exercise10.sql
@@ -0,0 +1,2 @@
+mysql> alter table Users add column MiddleName varchar(255), add column NickName
+ varchar(255), add column Email varchar(255),add column Suffix varchar(64), add column BadgeID varchar(255);
